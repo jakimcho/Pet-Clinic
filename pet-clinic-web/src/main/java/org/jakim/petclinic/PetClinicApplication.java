264eefcd18ae6bb6499ab199ba690af604176970
Yakim H. Rachev <jakimcho@gmail.com>
2019-08-25 09:14:06 +0300

    Convert the project t multi-module project. closes issue #3

--- pet-clinic-web/src/main/java/org/jakim/petclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/org/jakim/petclinic/PetClinicApplication.java
@@ -1,8 +1,6 @@
 package org.jakim.petclinic;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class PetClinicApplication {
