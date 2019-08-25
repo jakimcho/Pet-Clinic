5b66b430d2730daed63f9fefe2093c6a6de72225
Yakim H. Rachev <jakimcho@gmail.com>
2019-08-25 11:02:43 +0300

    Implement Interfaces for services. Closes #4

---
+++ pet-clinic-data/src/main/java/org/jakim/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package org.jakim.petclinic.services;
+
+import org.jakim.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
