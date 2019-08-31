34798ba4c89104ca91120fd4c04677fe096c834a
Yakim H. Rachev <jakimcho@gmail.com>
2019-08-31 19:49:21 +0300

    Create Vet Specialty Entity, Associated to Vet. closes #24

---
+++ pet-clinic-data/src/main/java/org.jakim.petclinic/model/Specialty.java
@@ -0,0 +1,16 @@
+package org.jakim.petclinic.model;
+
+public class Specialty extends BaseEntity
+{
+    private String description;
+
+    public String getDescription( )
+    {
+        return description;
+    }
+
+    public void setDescription( String description )
+    {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/org.jakim.petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org.jakim.petclinic/model/Vet.java
@@ -1,6 +1,18 @@
 package org.jakim.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+private Set<Specialty> specialties;
 
+    public Set<Specialty> getSpecialties( )
+    {
+        return specialties;
+    }
+
+    public void setSpecialties( Set<Specialty> specialties )
+    {
+        this.specialties = specialties;
+    }
 }
